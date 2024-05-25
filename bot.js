fbac536f19771face8fc92867cd9d81f33a4a518
Matthias Risto <m.risto@meddevo.com>
2024-05-25 09:52:08 +0200

    feat(deps): create threads, when answering user and other utilities

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { OpenAI } = require("openai");
+const {Client, GatewayIntentBits} = require('discord.js');
+const {OpenAI} = require("openai");
 require("dotenv").config();
 
 const openai = new OpenAI({
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 
 // Discord Client
 const client = new Client({
-  intents: [
+    intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
@@ -43,7 +43,7 @@ const statusCheckLoop = async (openAiThreadId, runId) => {
         runId
     );
 
-    if(terminalStates.indexOf(run.status) < 0){
+    if (terminalStates.indexOf(run.status) < 0) {
         await sleep(1000);
         return statusCheckLoop(openAiThreadId, runId);
     }
@@ -56,23 +56,81 @@ const addMessage = (threadId, content) => {
     // console.log(content);
     return openai.beta.threads.messages.create(
         threadId,
-        { role: "user", content }
+        {role: "user", content}
     )
+}
+
+function getTheAnswerIsBeingGenerated() {
+    return 'The answer is being generated...';
+}
+
+async function createThreadAndSayThatAnswerIsGenerated(message) {
+    if (!message.channel?.isThread()) {
+        if (message.hasThread) {
+            const thread = await message.thread;
+            await thread.send(getTheAnswerIsBeingGenerated());
+            return thread;
+        } else {
+
+            let trimmedContent = message.content;
+            if (trimmedContent.length > 60) {
+                trimmedContent = trimmedContent.substring(0, 60) + '...';
+            }
+            // Create a new thread and send a message
+            const thread = await message.startThread({
+                name: trimmedContent,
+                autoArchiveDuration: 60, // Dauer in Minuten, bis der Thread automatisch archiviert wird
+            });
+            await thread.send(getTheAnswerIsBeingGenerated());
+            return thread;
+        }
+    } else {
+        await message.reply(getTheAnswerIsBeingGenerated());
+        return message.channel;
+    }
+}
+
+async function sendReponseInChunksToDiscord(response, discordThread) {
+    const chunkSize = 1999;
+    let startIndex = 0;
+
+    while (startIndex < response.length) {
+        let endIndex = startIndex + chunkSize;
+
+        // Adjust endIndex to not split at the mid of sentence or list
+        if (endIndex < response.length) {
+            while (response[endIndex] !== '.' && response[endIndex] !== ',' && endIndex > startIndex) {
+                endIndex--;
+            }
+        }
+
+        const messageChunk = response.slice(startIndex, endIndex + 1);
+        await discordThread.send(messageChunk);
+        startIndex = endIndex + 1;
+    }
 }
 
 // This event will run every time a message is received
 client.on('messageCreate', async message => {
     if (message.author.bot || !message.content || message.content === '') return; //Ignore bot messages
     // console.log(message);
-    const discordThreadId = message.channel.id;
+    //
+
+    //
+    // console.log(openAiThreadId);
+
+    console.log("message thread",);
+    const discordThread = await createThreadAndSayThatAnswerIsGenerated(message);
+    const discordThreadId = discordThread.id;
     let openAiThreadId = getOpenAiThreadId(discordThreadId);
 
     let messagesLoaded = false;
-    if(!openAiThreadId){
+
+    if (!openAiThreadId) {
         const thread = await openai.beta.threads.create();
         openAiThreadId = thread.id;
         addThreadToMap(discordThreadId, openAiThreadId);
-        if(message.channel.isThread()){
+        if (message.channel.isThread()) {
             //Gather all thread messages to fill out the OpenAI thread since we haven't seen this one yet
             const starterMsg = await message.channel.fetchStarterMessage();
             const otherMessagesRaw = await message.channel.messages.fetch();
@@ -84,30 +142,25 @@ client.on('messageCreate', async message => {
             const messages = [starterMsg.content, ...otherMessages]
                 .filter(msg => !!msg && msg !== '')
 
-            // console.log(messages);
             await Promise.all(messages.map(msg => addMessage(openAiThreadId, msg)));
             messagesLoaded = true;
         }
     }
 
-    // console.log(openAiThreadId);
-    if(!messagesLoaded){ //If this is for a thread, assume msg was loaded via .fetch() earlier
+    if (!messagesLoaded) { //If this is for a thread, assume msg was loaded via .fetch() earlier
         await addMessage(openAiThreadId, message.content);
     }
 
     const run = await openai.beta.threads.runs.create(
         openAiThreadId,
-        { assistant_id: process.env.ASSISTANT_ID }
+        {assistant_id: process.env.ASSISTANT_ID}
     )
     const status = await statusCheckLoop(openAiThreadId, run.id);
 
     const messages = await openai.beta.threads.messages.list(openAiThreadId);
     let response = messages.data[0].content[0].text.value;
-    response = response.substring(0, 1999) //Discord msg length limit when I was testing
+    await sendReponseInChunksToDiscord(response, discordThread);
 
-    console.log(response);
-    
-    message.reply(response);
 });
 
 // Authenticate Discord
